18184bfba22f81fa478e8522947e77c959957689
Pablo Hernández <pablo.hernandez@nillion.com>
2023-01-25 12:06:07 +0000

    Integrate last changes. SourceRef support. The operations' fields are move to a specific struct.

--- scripts/build_virtualenv.sh
+++ scripts/build_virtualenv.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-pip install --user virtualenv ipykernel
 python3 -m venv venv
-python3 -m ipykernel install --user --name=venv
 source venv/bin/activate
 pip install -r requirements.txt
 deactivate
