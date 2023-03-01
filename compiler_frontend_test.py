cdcd58f6c0a6f77ce0d07233707c48e87fa89543
Matias Fontanini <matias.fontanini@gmail.com>
2023-03-01 09:49:45 -0800

    Rename nada's circuits to programs

--- compiler_frontend_test.py
+++ compiler_frontend_test.py
@@ -6,7 +6,9 @@ import pytest
 from deepdiff import DeepDiff
 from nada_dsl.compiler_frontend import nada_dsl_to_nada_mir
 
-tests_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"../tests/resources/circuits/current/")
+tests_directory = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), f"../tests/resources/programs/current/"
+)
 
 sys.path.insert(0, tests_directory)
 from addition_simple import outputs as addition_simple
@@ -19,39 +21,42 @@ from multiplication_simple import outputs as multiplication_simple
 from output_multiple import outputs as output_multiple
 from import_file import outputs as import_file
 
-mir_files_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"../tests/resources/mir/")
+mir_files_directory = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), f"../tests/resources/mir/"
+)
 
 testdata = [
-    (addition_simple, 'current.addition_simple'),
-    (circuit_simple, 'current.circuit_simple'),
-    (circuit_simple_2, 'current.circuit_simple_2'),
-    (empty, 'current.empty'),
-    (input_single, 'current.input_single'),
-    (integer, 'current.integer'),
-    (multiplication_simple, 'current.multiplication_simple'),
-    (output_multiple, 'current.output_multiple'),
-    (import_file, 'current.import_file'),
+    (addition_simple, "current.addition_simple"),
+    (circuit_simple, "current.circuit_simple"),
+    (circuit_simple_2, "current.circuit_simple_2"),
+    (empty, "current.empty"),
+    (input_single, "current.input_single"),
+    (integer, "current.integer"),
+    (multiplication_simple, "current.multiplication_simple"),
+    (output_multiple, "current.output_multiple"),
+    (import_file, "current.import_file"),
 ]
 
 test_ids = [
-    'addition_simple',
-    'circuit_simple',
-    'circuit_simple_2',
-    'empty',
-    'input_single',
-    'integer',
-    'multiplication_simple',
-    'output_multiple',
-    'import_file',
+    "addition_simple",
+    "circuit_simple",
+    "circuit_simple_2",
+    "empty",
+    "input_single",
+    "integer",
+    "multiplication_simple",
+    "output_multiple",
+    "import_file",
 ]
 
 
-@pytest.mark.parametrize(('outputs', 'expected_file_name'), testdata, ids=test_ids)
+@pytest.mark.parametrize(("outputs", "expected_file_name"), testdata, ids=test_ids)
 def test_compile_to_nada_mir(outputs, expected_file_name):
     mir_model = nada_dsl_to_nada_mir(outputs())
 
     file = open(f"{mir_files_directory}{expected_file_name}.nada-mir.json")
     expected_mir_model = json.load(file)
 
-    assert not DeepDiff(mir_model, expected_mir_model, view="tree", exclude_regex_paths="['source_ref']")
-
+    assert not DeepDiff(
+        mir_model, expected_mir_model, view="tree", exclude_regex_paths="['source_ref']"
+    )
