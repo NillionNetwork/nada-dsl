11989db766c5944244ef06ffab862b6133deae8c
Matias Fontanini <matias.fontanini@gmail.com>
2023-03-28 12:07:11 -0700

    Update all scripts to use new `nada_main` entrypoint

--- compiler_frontend_test.py
+++ compiler_frontend_test.py
@@ -11,35 +11,42 @@ tests_directory = os.path.join(
 )
 
 sys.path.insert(0, tests_directory)
-from addition_fixed_point_rational import outputs as addition_fixed_point_rational
-from addition_fixed_point_rational_error import outputs as addition_fixed_point_rational_error
-from addition_simple import outputs as addition_simple
-from circuit_simple import outputs as circuit_simple
-from circuit_simple_2 import outputs as circuit_simple_2
-from fixed_point_rational import outputs as fixed_point_rational
-from import_file import outputs as import_file
-from input_single import outputs as input_single
-from integer import outputs as integer
-from multiplication_fixed_point_rational import outputs as multiplication_fixed_point_rational
-from multiplication_simple import outputs as multiplication_simple
-from secret_string import outputs as secret_string
+from addition_fixed_point_rational import nada_main as addition_fixed_point_rational
+from addition_fixed_point_rational_error import (
+    nada_main as addition_fixed_point_rational_error,
+)
+from addition_simple import nada_main as addition_simple
+from circuit_simple import nada_main as circuit_simple
+from circuit_simple_2 import nada_main as circuit_simple_2
+from fixed_point_rational import nada_main as fixed_point_rational
+from import_file import nada_main as import_file
+from input_single import nada_main as input_single
+from integer import nada_main as integer
+from multiplication_fixed_point_rational import (
+    nada_main as multiplication_fixed_point_rational,
+)
+from multiplication_simple import nada_main as multiplication_simple
+from secret_string import nada_main as secret_string
 
 mir_files_directory = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), f"../tests/resources/mir/"
 )
 
 testdata = [
-    (addition_fixed_point_rational, "current.addition_fixed_point_rational"),
-    (addition_simple, "current.addition_simple"),
-    (circuit_simple, "current.circuit_simple"),
-    (circuit_simple_2, "current.circuit_simple_2"),
-    (fixed_point_rational, "current.fixed_point_rational"),
-    (import_file, "current.import_file"),
-    (input_single, "current.input_single"),
-    (integer, "current.integer"),
-    (multiplication_fixed_point_rational, "current.multiplication_fixed_point_rational"),
-    (multiplication_simple, "current.multiplication_simple"),
-    (secret_string, "current.secret_string"),
+    (addition_fixed_point_rational, "addition_fixed_point_rational"),
+    (addition_simple, "addition_simple"),
+    (circuit_simple, "circuit_simple"),
+    (circuit_simple_2, "circuit_simple_2"),
+    (fixed_point_rational, "fixed_point_rational"),
+    (import_file, "import_file"),
+    (input_single, "input_single"),
+    (integer, "integer"),
+    (
+        multiplication_fixed_point_rational,
+        "multiplication_fixed_point_rational",
+    ),
+    (multiplication_simple, "multiplication_simple"),
+    (secret_string, "secret_string"),
 ]
 
 test_ids = [
