a4518316d264b726a5111e42cafbe92e94a9c45f
Jonathan Mercier-Ganady <jmgr@jmgr.info>
2023-05-16 08:53:34 +0100

    Remove secret boolean Equals operation since it has not been implemented yet

--- nada_types/boolean.py
+++ nada_types/boolean.py
@@ -8,12 +8,7 @@ from nada_dsl.source_ref import SourceRef
 
 @dataclass
 class SecretBoolean(NadaType):
-    def __eq__(self, other: "SecretBoolean") -> "SecretBoolean":
-        operation = Equals(left=self, right=other, source_ref=SourceRef.back_frame())
-        if isinstance(other, SecretBoolean):
-            return SecretBoolean(inner=operation)
-        else:
-            raise TypeError(f"Invalid operation: {self} == {other}")
+    pass
 
 
 @dataclass
