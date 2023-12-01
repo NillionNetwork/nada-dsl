3244ee0b0c4e0cef840d206fc04fb022788aa7a0
Juan M Salamanca <juan.salamanca@nillion.com>
2023-12-01 17:52:43 +0000

    pynadac: Compile NADA string

--- nada_dsl/compile.py
+++ nada_dsl/compile.py
@@ -1,12 +1,18 @@
 import sys
 import os.path
+import base64
 import json
 from dataclasses import dataclass
 import traceback
+import importlib.util
+import nada_dsl
+from nada_dsl.compiler_frontend import nada_compile
 
 
 @dataclass
 class CompileOutput:
+    """ Compiler Output
+    """
     mir: str
     nada_dsl_path: str
 
@@ -20,9 +26,6 @@ def transform_program(script_path: str) -> CompileOutput:
 
     script = __import__(script_name)
 
-    import nada_dsl
-    from nada_dsl.compiler_frontend import nada_compile
-
     try:
         main = getattr(script, "nada_main")
     except:
@@ -33,17 +36,55 @@ def transform_program(script_path: str) -> CompileOutput:
     return CompileOutput(compile_output, nada_dsl_path)
 
 
-if __name__ == "__main__":
-    try:
-        if len(sys.argv) != 2:
-            raise Exception("expected program as argument")
-        output = transform_program(sys.argv[1])
-        output = {
+def compile_string(script: str) -> CompileOutput:
+    """Compiles a NADA program as string
+
+    Args:
+        script (str): The nada program as a base64 encoded string (UTF-8)
+
+    Returns:
+        CompileOutput: The Compiler Output
+    """
+    decoded_program = base64.b64decode(script).decode('utf-8')
+    temp_name = 'temp_program'
+    spec = importlib.util.spec_from_loader(temp_name, loader=None)
+    module = importlib.util.module_from_spec(spec)
+    exec(decoded_program, module.__dict__)
+    sys.modules[temp_name] = module
+    globals()[temp_name] = module
+
+    
+    outputs = module.nada_main()
+    compile_output = nada_compile(outputs)
+    nada_dsl_path = nada_dsl.__path__[0]
+    return CompileOutput(compile_output, nada_dsl_path)
+
+
+def print_output(output: CompileOutput): 
+    """Prints compiler output
+
+    Args:
+        output (CompileOutput): Output of the compiler
+    """
+    output_json = {
             "result": "Success",
             "mir": output.mir,
             "nada_dsl_path": output.nada_dsl_path,
         }
-        print(json.dumps(output))
+    print(json.dumps(output_json))
+
+if __name__ == "__main__":
+    try:
+        args_length = len(sys.argv)
+        if args_length < 2:
+            raise Exception("expected program as argument")
+        if args_length == 2:
+            output = transform_program(sys.argv[1])
+            print_output(output)
+        if args_length == 3 and sys.argv[1] == '-s':
+            output = compile_string(sys.argv[2]) 
+            print_output(output)
+
     except Exception as ex:
         tb = traceback.format_exc()
         output = {"result": "Failure", "reason": str(ex), "traceback": str(tb)}
