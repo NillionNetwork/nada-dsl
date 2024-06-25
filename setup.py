ef82f6f746413d1d9d14f1bf6d6d127d42e0b6cd
Juan M Salamanca <juan.salamanca@nillion.com>
2024-06-25 16:04:12 +0100

    test: Add functional tests for public variables

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
-from setuptools import setup
 import os
+from setuptools import setup
+
+VERSION_FILE = "./nada_dsl/version.py"
 
 
-# VERSION GENERATION
-# This script is run multiple times for any wheels in temp folders
-# We only want `version.py` to be created once.
-if not os.path.isfile("./nada_dsl/version.py"):
+def get_head_commit() -> str:
+    """Returns the git HEAD commit"""
+    return os.popen("git rev-parse HEAD").read().rstrip()
+
+
+def require_version_file_update() -> bool:
+    """Update version file when:
+    - It doesn't exist, or
+    - The version doesn't match the head commit.
+    """
+    if os.path.isfile(VERSION_FILE):
+        with open(VERSION_FILE, "r", encoding="utf-8") as file:
+            nada_dsl_commit = file.read().rstrip().split(" ")[1][:-2]
+            return get_head_commit() != nada_dsl_commit
+    return True
+
+
+if require_version_file_update():
     commit = os.popen("git rev-parse HEAD").read().rstrip()
-    print(f"COMMIT: {commit}")
-    with open("./nada_dsl/version.py", "w", encoding="utf-8") as f:
+    with open(VERSION_FILE, "w", encoding="utf-8") as f:
         f.write(f"print('nada_dsl {commit}')")
 
 setup()
