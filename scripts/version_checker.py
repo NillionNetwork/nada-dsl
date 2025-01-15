7c77ce8ed8f13407858b0f2b9eb20dc3af023746
Pablo Hernández <37065993+pablojhl@users.noreply.github.com>
2025-01-15 10:02:33 +0000

    chore: update version_checker.py to publish release candidate versions (#70)

--- scripts/version_checker.py
+++ scripts/version_checker.py
@@ -6,15 +6,12 @@ import tomli
 import os
 import requests
 from packaging import version
+from packaging.version import Version
 import json
 
-from packaging.version import Version
 
-
-def get_public_version(project_name: str, is_test=False) -> Version:
-    response = requests.get(
-        f'https://{"test." if is_test else ""}pypi.org/pypi/{project_name}/json'
-    )
+def get_public_version(project_name: str) -> Version:
+    response = requests.get(f"https://pypi.org/pypi/{project_name}/json")
     if response.status_code == 200:
         return version.parse(json.loads(response.content)["info"]["version"])
     else:
@@ -23,16 +20,11 @@ def get_public_version(project_name: str, is_test=False) -> Version:
 
 if __name__ == "__main__":
     pyproject_toml_path = sys.argv[1]
-    test_regex = sys.argv[2] if len(sys.argv) > 2 else None
     with open(pyproject_toml_path, "rb") as f:
         project = tomli.load(f)
 
     project_version = version.parse(project["project"]["version"])
-    is_test = False
-    if test_regex:
-        if re.compile(test_regex).search(str(project_version)):
-            is_test = True
-    public_project_version = get_public_version(project["project"]["name"], is_test)
+    public_project_version = get_public_version(project["project"]["name"])
 
     with open(os.environ["GITHUB_OUTPUT"], "at") as f:
         f.write(
@@ -40,4 +32,3 @@ if __name__ == "__main__":
         )
         f.write(f"local_version={str(project_version)}\n")
         f.write(f"public_version={str(public_project_version)}\n")
-        f.write(f"is_test={str(is_test).lower()}\n")
